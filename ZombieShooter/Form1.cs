bfd18065760637e960a038b1db314150d14a4a65
awillsea <Alexanderwillsea@gmail.com>
2023-02-16 14:26:28 -0700

    test

--- ZombieShooter/Form1.cs
+++ ZombieShooter/Form1.cs
@@ -6,5 +6,33 @@ namespace ZombieShooter
         {
             InitializeComponent();
         }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyisDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyisUp(object sender, KeyEventArgs e)
+        {
+
+        }
+        public void ShootBullet(string direction)
+        {
+
+        }
+        public void MakeZombies()
+        {
+            int no = 0;
+
+        } //hey
+        public void RestartGame()
+        {
+
+        }
     }
 }
